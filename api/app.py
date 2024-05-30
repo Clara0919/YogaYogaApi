6a025f859078742f0fd70e4bb288bda6226203d6
Clara He <clara.he@newtype.com.tw>
2024-05-30 17:00:35 +0800

    fix: 修正檔案結構以符合 Vercel 配置

--- api/app.py
+++ api/app.py
@@ -1,15 +1,14 @@
-from flask import Flask, request
-import getYogaData
+from flask import Flask, request, jsonify
+import api.getYogaData as getYogaData
 from flask_cors import CORS
 
 app = Flask(__name__)
-
-
 CORS(app)  # 啟用 CORS
+
 @app.route("/getAllBranch", methods=['GET'])
 def getAllBranch():
     result =getYogaData.formatted_branchData
-    return result
+    return jsonify(result)
 
 @app.route("/getFilterData", methods=['GET'])
 def getFilterData():
@@ -18,5 +17,7 @@ def getFilterData():
     date= request.args.get('date')
     view = request.args.get('view')
     result=getYogaData.getFilterClassData(branchId,tab,date,view)
+    return jsonify(result)
 
-    return result 
+if __name__ == "__main__":
+    app.run()
